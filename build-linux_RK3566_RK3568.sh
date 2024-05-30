1daacffbb31ed0965adbdecdc3a8168ef9af7258
heng <tickley>
2024-05-30 20:24:31 +0800

    camera rtsp 延迟160-180ms

---
+++ build-linux_RK3566_RK3568.sh
@@ -0,0 +1,23 @@
+set -e
+
+#GCC_COMPILER=aarch64-linux-gnu
+GCC_COMPILER=/home/heng/rk356x_1.4.0_20231220/buildroot/output/rockchip_rk3568/host/bin/aarch64-buildroot-linux-gnu
+
+export LD_LIBRARY_PATH=${TOOL_CHAIN}/lib64:$LD_LIBRARY_PATH
+export CC=${GCC_COMPILER}-gcc
+export CXX=${GCC_COMPILER}-g++
+
+ROOT_PWD=$( cd "$( dirname $0 )" && cd -P "$( dirname "$SOURCE" )" && pwd )
+
+# build
+BUILD_DIR=${ROOT_PWD}/build/build_linux_aarch64
+
+if [[ ! -d "${BUILD_DIR}" ]]; then
+  mkdir -p ${BUILD_DIR}
+fi
+
+cd ${BUILD_DIR}
+cmake ../.. -DCMAKE_SYSTEM_NAME=Linux 
+make -j8
+make install
+cd -
